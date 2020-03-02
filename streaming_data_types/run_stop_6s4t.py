e7fc3d6886a7b09ac9425490ef138419fb733dd8
Matthew D Jones <matthew.d.jones@tessella.com>
2020-03-02 14:01:34 +0000

    Add deserialisation for run stop

--- streaming_data_types/run_stop_6s4t.py
+++ streaming_data_types/run_stop_6s4t.py
@@ -1,6 +1,10 @@
 from typing import Optional
 import flatbuffers
 from streaming_data_types.fbschemas.run_stop_6s4t import RunStop
+from streaming_data_types.utils import check_schema_identifier
+from collections import namedtuple
+
+FILE_IDENTIFIER = b"6s4t"
 
 
 def serialise_6s4t(
@@ -32,5 +36,18 @@ def serialise_6s4t(
 
     # Generate the output and replace the file_identifier
     buff = builder.Output()
-    buff[4:8] = b"6s4t"
+    buff[4:8] = FILE_IDENTIFIER
     return bytes(buff)
+
+
+def deserialise_6s4t(buffer):
+    check_schema_identifier(buffer, FILE_IDENTIFIER)
+
+    run_stop = RunStop.RunStop.GetRootAsRunStop(buffer, 0)
+    service_id = run_stop.ServiceId() if run_stop.ServiceId() else b""
+    job_id = run_stop.JobId() if run_stop.JobId() else b""
+    run_name = run_stop.RunName() if run_stop.RunName() else b""
+    stop_time = run_stop.StopTime()
+
+    RunStopInfo = namedtuple("RunStopInfo", "stop_time run_name job_id service_id")
+    return RunStopInfo(stop_time, run_name, job_id, service_id)
