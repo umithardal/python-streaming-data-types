a5460a96434e51e0c3b5dac1aa43a24cc177f958
Matthew D Jones <matthew.d.jones@tessella.com>
2020-07-10 15:06:49 +0100

    Avoid nesting builders during serialisation

--- streaming_data_types/forwarder_config_update_rf5k.py
+++ streaming_data_types/forwarder_config_update_rf5k.py
@@ -29,6 +29,14 @@ def deserialise_rf5k(buffer):
     return ConfigurationUpdate(config_message.ConfigChange(), [])
 
 
+def serialise_stream(builder, topic_offset, schema_offset, channel_offset):
+    Stream.StreamStart(builder)
+    Stream.StreamAddTopic(builder, topic_offset)
+    Stream.StreamAddSchema(builder, schema_offset)
+    Stream.StreamAddChannel(builder, channel_offset)
+    return Stream.StreamEnd(builder)
+
+
 def serialise_rf5k(config_change: UpdateType, streams: List[StreamInfo]) -> bytes:
     """
     Serialise config update message as an rf5k FlatBuffers message.
@@ -37,29 +45,35 @@ def serialise_rf5k(config_change: UpdateType, streams: List[StreamInfo]) -> byte
     :param streams: channel, schema and output topic configurations
     :return:
     """
-
     builder = flatbuffers.Builder(1024)
 
     if streams:
-        streams_offset = ConfigUpdate.ConfigUpdateStartStreamsVector(
-            builder, len(streams)
-        )
-        for stream in streams:
-            channel_offset = builder.CreateString(stream.channel)
-            schema_offset = builder.CreateString(stream.schema)
-            topic_offset = builder.CreateString(stream.topic)
-            Stream.StreamStart(builder)
-            Stream.StreamAddTopic(builder, topic_offset)
-            Stream.StreamAddSchema(builder, schema_offset)
-            Stream.StreamAddChannel(builder, channel_offset)
-            stream_offset = Stream.StreamEnd(builder)
+        # We have to use multiple loops/list comprehensions here because we cannot create strings after we have
+        # called StreamStart and cannot create streams after we have called StartVector
+        stream_field_offsets = [
+            (
+                builder.CreateString(stream.channel),
+                builder.CreateString(stream.schema),
+                builder.CreateString(stream.topic),
+            )
+            for stream in streams
+        ]
+        stream_offsets = [
+            serialise_stream(builder, *stream_fields)
+            for stream_fields in stream_field_offsets
+        ]
+
+        ConfigUpdate.ConfigUpdateStartStreamsVector(builder, len(streams))
+        for stream_offset in stream_offsets:
             builder.PrependUOffsetTRelative(stream_offset)
-        ConfigUpdate.ConfigUpdateAddStreams(builder, streams_offset)
+        streams_offset = builder.EndVector(len(streams))
 
     # Build the actual buffer
     ConfigUpdate.ConfigUpdateStart(builder)
-    data = ConfigUpdate.ConfigUpdateEnd(builder)
+    if streams:
+        ConfigUpdate.ConfigUpdateAddStreams(builder, streams_offset)
     ConfigUpdate.ConfigUpdateAddConfigChange(builder, config_change)
+    data = ConfigUpdate.ConfigUpdateEnd(builder)
     builder.Finish(data)
 
     # Generate the output and replace the file_identifier
