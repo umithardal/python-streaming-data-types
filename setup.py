72a05e048c09185e2456d1e93aa68d5a5494571a
Matthew D Jones <matthew.d.jones@tessella.com>
2020-07-16 15:45:42 +0100

    Bump version to 0.9.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except Exception as error:
 
 setup(
     name="ess_streaming_data_types",
-    version="0.8.1",
+    version="0.9.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
